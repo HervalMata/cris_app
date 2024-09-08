09f0b58fc8fcb9b746589aa74e0d67eeedde069c
HervalMata <hervalmataamparo@gmail.com>
2024-09-08 10:49:47 -0300

    feat:art: cris app - create wishlist screen

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,5 +1,6 @@
 import 'package:cris_app/features/shop/screens/home.dart';
 import 'package:cris_app/features/shop/screens/store/store.dart';
+import 'package:cris_app/features/shop/screens/wishlist/wishlist.dart';
 import 'package:cris_app/utils/constants/colors.dart';
 import 'package:cris_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
@@ -42,7 +43,7 @@ class NavigationController extends GetxController {
   final screens = [
     const HomeScreen(),
     const StoreScreen(),
-    Container(color: Colors.orange,),
+    const FavouriteScreen(),
     Container(color: Colors.blue,),
   ];
 }
